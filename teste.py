bbc19d0454524cbfc690942d4aaea995aceba00d
HenriqueLafa <henriquelafayete39@gmail.com>
2025-05-05 09:35:52 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,8 @@
+# Imprimir uma mensagem
+print("Olá! Bem-vindo ao mundo do Python.")
+
+# Pedir o nome do usuário
+nome = input("Qual é o seu nome? ")
+
+# Responder com uma saudação personalizada
+print(f"Prazer em te conhecer, {nome}!")
